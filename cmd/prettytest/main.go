1351a415345c59c59a73542535e0285027c95db1
Raúl Garrido <me@happybydefault.com>
2021-12-26 20:31:19 -0300

    Improve prettytest

--- cmd/prettytest/main.go
+++ cmd/prettytest/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -9,35 +13,6 @@ import (
 
 	"github.com/gookit/color"
 )
-
-type prettifier struct {
-}
-
-func (w *prettifier) Write(p []byte) (n int, err error) {
-	lines := strings.Split(string(p), "\n")
-	for _, line := range lines {
-		trimmed := strings.TrimLeft(line, " ")
-		switch {
-		case line == "PASS":
-			color.Greenln(line)
-		case line == "FAIL":
-			color.Redln(line)
-		case strings.HasPrefix(line, "FAIL"):
-			color.Redln(line + " ✘")
-		case strings.HasPrefix(line, "ok "):
-			color.Greenln(line + " ✓")
-		case strings.HasPrefix(trimmed, "--- PASS: "):
-			line = strings.Replace(line, "--- PASS: ", "", 1)
-			color.Greenln(line + " ✓")
-		case strings.HasPrefix(trimmed, "--- FAIL: "):
-			line = strings.Replace(line, "--- FAIL: ", "", 1)
-			color.Redln(line + " ✘")
-		default:
-			fmt.Println(line)
-		}
-	}
-	return len(p), err
-}
 
 func main() {
 	args := append([]string{"test"}, os.Args[1:]...)
@@ -58,4 +33,69 @@ func main() {
 		log.Printf("error while running command: %v", err)
 		os.Exit(1)
 	}
+}
+
+type prettifier struct{}
+
+func (p *prettifier) Write(data []byte) (int, error) {
+	if len(data) == 0 {
+		return 0, nil
+	}
+
+	r := bufio.NewReader(bytes.NewReader(data))
+
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return 0, err
+		}
+
+		line = pretty(line)
+		fmt.Print(line)
+	}
+
+	return len(data), nil
+}
+
+func pretty(line string) string {
+	trimmed := strings.TrimLeft(line, " ")
+
+	switch {
+	case line == "PASS\n":
+		return color.Green.Sprint(line)
+	case line == "FAIL\n":
+		return color.Red.Sprint(line)
+	case
+		strings.HasPrefix(line, "ok "),
+		strings.HasPrefix(trimmed, "--- PASS: "):
+
+		line = appendToLine(line, "✓")
+		return color.Green.Sprint(line)
+	case
+		strings.HasPrefix(trimmed, "--- FAIL: "),
+		strings.HasPrefix(line, "FAIL"):
+
+		line = appendToLine(line, "✘")
+		return color.Red.Sprint(line)
+	}
+
+	return line
+}
+
+func appendToLine(line string, suffix string) string {
+	hasLineBreak := strings.HasSuffix(line, "\n")
+	if hasLineBreak {
+		line = strings.TrimSuffix(line, "\n")
+	}
+
+	line = fmt.Sprintf("%s %s", line, suffix)
+
+	if hasLineBreak {
+		line = line + "\n"
+	}
+
+	return line
 }
